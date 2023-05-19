c62a1d92b6842d23af207920494355bb4d93ecfa
tamanchichan <tamanchichan@gmail.com>
2023-05-19 12:21:35 -0500

    HashRouter instead of BrowserRouter

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import './index.css';
 import App from './App';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
